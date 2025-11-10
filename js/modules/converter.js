3c74e3d5fac04576aa95eb8ba66f661f4a5e3ada
akmalworkfront <akmal.work.front@gmail.com>
2025-11-10 22:09:52 +0600

    fix

--- js/modules/converter.js
+++ js/modules/converter.js
@@ -9,17 +9,17 @@ const converter = (element) => {
       const data = await response.json();
       const usd = data.usd;
       const eur = data.eur;
-      const flatDot = 2
+      const floatDot = 2
 
       if (element.id === 'som') {
-        usdInput.value = (somInput.value / usd).toFixed(flatDot);
-        eurInput.value = (somInput.value / eur).toFixed(flatDot);
+        usdInput.value = (somInput.value / usd).toFixed(floatDot);
+        eurInput.value = (somInput.value / eur).toFixed(floatDot);
       } else if (element.id === 'usd') {
-        somInput.value = (usdInput.value * usd).toFixed(flatDot);
-        eurInput.value = ((usdInput.value * usd) / eur).toFixed(flatDot);
+        somInput.value = (usdInput.value * usd).toFixed(floatDot);
+        eurInput.value = ((usdInput.value * usd) / eur).toFixed(floatDot);
       } else if (element.id === 'eur') {
-        somInput.value = (eurInput.value * eur).toFixed(flatDot);
-        usdInput.value = ((eurInput.value * eur) / usd).toFixed(flatDot);
+        somInput.value = (eurInput.value * eur).toFixed(floatDot);
+        usdInput.value = ((eurInput.value * eur) / usd).toFixed(floatDot);
       }
     } catch (error) {
       console.error('Ошибка при конвертации:', error);
